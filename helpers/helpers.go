da8222a2c3d6fe58c912a6dbd556d26c15b28dd7
Sergio Pereira <sergitopereira@hotmail.com>
2022-05-22 18:20:52 -0500

    updated typo parser

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -6,9 +6,9 @@ import (
 
 func TerminalHelper(args []string) {
 	if len(args) > 3 {
-		log.Fatal("Program requires TEST and OUPUT Folders")
+		log.Fatal("Program requires TEST DATA and OUTPUT fodler path")
 	} else if len(args) <= 2 {
-		log.Fatal("Program requires TEST and OUPUT  Folders")
+		log.Fatal("Program requires TEST DATA and OUTPUT folders path")
 	} else {
 		return
 	}
